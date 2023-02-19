430147ad0ae8df72977bdf7f924701e2b488e88c
loranYousef <loganyousef@gmail.com>
2023-02-19 09:22:28 +0100

    work on jscode

--- JS/todo.js
+++ JS/todo.js
@@ -13,17 +13,54 @@ const newTodo = document.createElement('h3');
 newTodo.innerText = todoInput.value ;
 todoDiv.appendChild(newTodo);
 newTodo.classList.add('todo-title')
+// remove data from input
+
+todoInput.value =''
 
 // create buttons div 
 const buttonDiv = document.createElement('div')
 todoDiv.appendChild(buttonDiv)
 
 // add complete , delete buttons 
-const   
+const completeBtn = document.createElement('button') 
+completeBtn.innerHTML = '<i class="fa-solid fa-check todo-check todo-icon"></i>'
+buttonDiv.appendChild(completeBtn)
+
+// add remove buttons
+const removeBtn = document.createElement('button') 
+removeBtn.innerHTML = '<i class="fa-solid fa-trash todo-delete  todo-icon"></i>'
+buttonDiv.appendChild(removeBtn)
+
+todoResult.appendChild(todoDiv);
+}
+function toggleTodo(e){
+    const item = e.target
+    
+    
+
+    if (item.classList[3] == 'todo-check'){
+        const btn_div = item.parentElement.parentElement.parentElement
+        //btn_div.classList.add('completed-todo')
+        btn_div.childNodes[0].classList.add('completed-todo')
+        
+
+    }
+
+    if (item.classList[3] == 'todo-delete'){
+        const btn_div = item.parentElement.parentElement.parentElement
+        btn_div.classList.add('remove-todo')
+
+    }
+        
+
+
+    
+    
 
 }
 
 
 todoBtn.addEventListener('click',addTodo)
+todoResult.addEventListener('click',toggleTodo)
 
  
